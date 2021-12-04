bdc38e3187da425d94a33fcdcde03d60c1a38169
Charles Mariot <contact@charles-mariot.fr>
2021-12-04 18:14:26 +0100

    Better prompt, need to check valgrind and check the arguments of the pwd builtin [4 déc. 2021 à 18:13]

--- srcs/main.c
+++ srcs/main.c
@@ -6,41 +6,47 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 21:25:55 by cmariot           #+#    #+#             */
-/*   Updated: 2021/12/04 16:35:55 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/12/04 18:11:26 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*get_promt(t_shell *ministruct)
+char	*get_prompt(t_shell *ministruct)
 {
 	int		len;
 	int		i;
 	char	*current_directory;
+	char	*prompt;
 
 	i = ft_strlen(ministruct->pwd);
+	if (i == 1)
+		return (ft_strdup("/: "));
 	len = 0;
-	while (ministruct->pwd[i--] != '/')
+	while (ministruct->pwd[i--] != '/' && i != 0)
 		len++;
-	current_directory = malloc(sizeof(char) * len - 1);
+	current_directory = malloc(sizeof(char) * len + 1);
 	if (!current_directory)
 		return (ft_strdup("Default prompt: "));
-	printf("malloc de %d, cd[%d] = \\0\n", len - 1, len - 1);
 	i = ft_strlen(ministruct->pwd);
-	current_directory[len - 2] = '\0';
+	current_directory[len - 1] = '\0';
 	while (len - 1 > 0)
 	{
 		current_directory[len - 2] = ministruct->pwd[i - 1];
 		i--;
 		len--;
 	}
-	return (current_directory);
+	prompt = ft_strjoin(current_directory, ": ");
+	free(current_directory);
+	return (prompt);
 }
 
 void	init_ministruct(char **env, t_shell *ministruct)
 {
 	ministruct->pwd = get_env("PWD=", env);
-	ministruct->prompt = get_promt(ministruct);
+	if (ministruct->prompt != NULL)
+		free(ministruct->prompt);
+	ministruct->prompt = get_prompt(ministruct);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -49,6 +55,7 @@ int	main(int argc, char **argv, char **env)
 
 	if (argc && *argv && *env)
 	{
+		ministruct.prompt = NULL;
 		while (1)
 		{
 			init_ministruct(env, &ministruct);
@@ -59,6 +66,7 @@ int	main(int argc, char **argv, char **env)
 			{
 				free(ministruct.line);
 				free(ministruct.pwd);
+				free(ministruct.prompt);
 				break ;
 			}
 			free(ministruct.line);
