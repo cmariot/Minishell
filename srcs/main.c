d1dce0c729043e5e3de1fbf34311d226ae564f7b
Charles Mariot <contact@charles-mariot.fr>
2021-12-06 09:00:28 +0100

    Prompt is the current directory. [6 déc. 2021 à 08:59]

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 21:25:55 by cmariot           #+#    #+#             */
-/*   Updated: 2021/12/04 18:11:26 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/12/06 08:59:14 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,23 @@ char	*get_prompt(t_shell *ministruct)
 	char	*prompt;
 
 	i = ft_strlen(ministruct->pwd);
-	if (i == 1)
-		return (ft_strdup("/: "));
+	if (i == 1 && ft_strcmp(ministruct->pwd, "/") == 0)
+		return (ft_strdup("/ ➤ "));
 	len = 0;
-	while (ministruct->pwd[i--] != '/' && i != 0)
+	while (ministruct->pwd[i--] != '/')
 		len++;
-	current_directory = malloc(sizeof(char) * len + 1);
+	current_directory = malloc(sizeof(char) * len);
 	if (!current_directory)
 		return (ft_strdup("Default prompt: "));
-	i = ft_strlen(ministruct->pwd);
 	current_directory[len - 1] = '\0';
-	while (len - 1 > 0)
+	i = ft_strlen(ministruct->pwd);
+	while (len - 1 != 0)
 	{
 		current_directory[len - 2] = ministruct->pwd[i - 1];
-		i--;
 		len--;
+		i--;
 	}
-	prompt = ft_strjoin(current_directory, ": ");
+	prompt = ft_strjoin(current_directory, " ➤ ");
 	free(current_directory);
 	return (prompt);
 }
