7bbafdb1da9d57acdfcb7c0c2272460bfa0dfc3d
Charles MARIOT <contact@charles-mariot.fr>
2021-12-23 14:44:38 +0100

    HOHOHOH

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 17:11:59 by cmariot           #+#    #+#             */
-/*   Updated: 2021/12/23 11:37:57 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/12/23 14:43:52 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@
  * followed by blank-separated words and redirections,
  * and terminated by a control operator.
  * 
- * The first word specifies the command to be executed, and is passed as argument zero.
- * The remaining words are passed as arguments to the invoked command.
- * The return value of a simple command is its exit status,
+ * The first word specifies the command to be executed, and is passed as
+ * argument zero. The remaining words are passed as arguments to the invoked
+ * command. The return value of a simple command is its exit status,
  * or 128+n if the command is terminated by signal n. */
 size_t	count_commands(char **splitted_line)
 {
@@ -39,6 +39,50 @@ size_t	count_commands(char **splitted_line)
 	return (number_of_commands);
 }
 
+int	fill_commands(t_simple command, char **splitted_line, size_t array_index)
+{
+	int	i;
+	int	len;
+
+	len = array_index;
+	while (splitted_line[len])
+	{
+		if (ft_strcmp(splitted_line[len], "|") == 0)
+			break ;
+		else if (ft_strcmp(splitted_line[len], ";") == 0)
+			break ;
+		len++;
+	}
+	printf("LEN = %d\n", len);
+	while (splitted_line[array_index])
+	{
+		printf("splitted_line[%lu] = %s\n", array_index, splitted_line[array_index]);
+		array_index++;
+	}
+	return (array_index);
+}
+
+void	get_simple_commands(t_command_line *command_line, char **splitted_line)
+{
+	size_t	i;
+	size_t	array_index;
+
+	command_line->number_of_simple_commands
+		= count_commands(command_line->splitted_line);
+	printf("NUMBER OF SIMPLE COMMANDS = %lu\n",
+		command_line->number_of_simple_commands);
+	command_line->command
+		= ft_calloc(command_line->number_of_simple_commands + 1,
+			sizeof(t_simple));
+	array_index = 0;
+	i = 0;
+	while (i < command_line->number_of_simple_commands)
+	{
+		fill_commands(command_line->command[i], splitted_line, array_index);
+		i++;
+	}
+}
+
 int	parse(t_command_line *command_line, t_shell *minishell)
 {
 	if (!check_quote(command_line->line)
@@ -51,9 +95,7 @@ int	parse(t_command_line *command_line, t_shell *minishell)
 			return (-1);
 		expand_env_variable(&command_line->splitted_line,
 			minishell->env);
-		command_line->number_of_simple_commands
-			= count_commands(command_line->splitted_line);
-		printf("NUMBER OF SIMPLE COMMANDS = %lu\n", command_line->number_of_simple_commands);
+		get_simple_commands(command_line, command_line->splitted_line);
 	}
 	return (0);
 }
