b2211bb39cef9b5e15222353e956e92b6a27aefc
Charles Mariot <contact@charles-mariot.fr>
2022-01-23 17:26:02 +0100

    Les exit status des commandes du pipe sont ok [23 janv. 2022 à 17:25]

--- srcs/execution/pipeline.c
+++ srcs/execution/pipeline.c
@@ -6,67 +6,63 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 13:56:14 by cmariot           #+#    #+#             */
-/*   Updated: 2022/01/23 14:13:22 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/01/23 17:24:09 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	fork_error(void)
+int	fork_error(void)
 {
 	print(2, "minishell: error: fork failed.\n");
+	return (1);
 }
 
-void	pipe_exit_status(int pid)
+int	pipe_error(void)
 {
-	if (WIFEXITED(pid))
-		global_exit_status(WEXITSTATUS(pid));
-	if (WIFSIGNALED(pid))
-	{
-		global_exit_status(WTERMSIG(pid));
-		if (return_global_exit_status() != 131)
-			global_exit_status(return_global_exit_status() + 128);
-	}
+	print(2, "minishell: error: pipe failed.\n");
+	return (1);
 }
 
 /* For the last pipe, STDIN is already redirect, so just fork.
  * In the child, execute command and exit.
  * In the parent, restore the file_descriptor */
-void	last_pipe(t_shell *minishell, t_simple command, int *backup_fd)
+int	last_pipe(t_shell *minishell, t_simple command, int *backup_fd)
 {
+	int	status;
+
 	if (pipe(command.pipe_fd) == -1)
-	{
-		print(2, "minishell: error: pipe failed.\n");
-		return ;
-	}
+		return (pipe_error());
 	command.pid = fork();
 	if (command.pid == -1)
 		return (fork_error());
 	else if (command.pid == 0)
 	{
 		execute_simple_command(minishell, command, backup_fd);
-		print(2, "EXIT_STATUS = %d\n", return_global_exit_status());
+		global_exit_status(return_global_exit_status());
+		status = return_global_exit_status();
 		close(command.pipe_fd[0]);
 		close(command.pipe_fd[1]);
 		close(backup_fd[0]);
 		close(backup_fd[1]);
 		free_minishell(minishell);
-		exit(return_global_exit_status());
 	}
+	wait(&status);
 	close(command.pipe_fd[1]);
 	dup2(command.pipe_fd[0], STDIN);
 	close(command.pipe_fd[0]);
+	return (status);
 }
 
 /* For the firsts pipes, create a pipe and fork the program
  * In the child redirect output into the pipe, execute command and exit.
  * In the parent, close the fd and redirect STDIN. */
-void	firsts_pipes(t_shell *minishell, t_simple command, int *backup_fd)
+int	firsts_pipes(t_shell *minishell, t_simple command, int *backup_fd)
 {
 	if (pipe(command.pipe_fd) == -1)
 	{
 		print(2, "minishell: error: pipe failed.\n");
-		return ;
+		return (1);
 	}
 	command.pid = fork();
 	if (command.pid == -1)
@@ -76,16 +72,15 @@ void	firsts_pipes(t_shell *minishell, t_simple command, int *backup_fd)
 		close(command.pipe_fd[0]);
 		dup2(command.pipe_fd[1], STDOUT);
 		execute_simple_command(minishell, command, backup_fd);
-		print(2, "EXIT_STATUS = %d\n", return_global_exit_status());
 		close(command.pipe_fd[1]);
 		close(backup_fd[0]);
 		close(backup_fd[1]);
 		free_minishell(minishell);
-		exit(return_global_exit_status());
 	}
 	close(command.pipe_fd[1]);
 	dup2(command.pipe_fd[0], STDIN);
 	close(command.pipe_fd[0]);
+	return (0);
 }
 
 /* For each simple command, connect the input and the output to the pipe,
@@ -94,6 +89,7 @@ void	create_pipeline(t_command_line *command_line, t_shell *minishell,
 		int *backup_fd)
 {
 	size_t	i;
+	int		status;
 
 	i = 0;
 	while (i < command_line->number_of_simple_commands)
@@ -101,7 +97,7 @@ void	create_pipeline(t_command_line *command_line, t_shell *minishell,
 		if (i < command_line->number_of_simple_commands - 1)
 			firsts_pipes(minishell, command_line->command[i], backup_fd);
 		else
-			last_pipe(minishell, command_line->command[i], backup_fd);
+			status = last_pipe(minishell, command_line->command[i], backup_fd);
 		i++;
 	}
 	i = 0;
@@ -111,5 +107,8 @@ void	create_pipeline(t_command_line *command_line, t_shell *minishell,
 			&(command_line->command[i].pid), 0);
 		i++;
 	}
-	//pipe_exit_status(command_line->command[i].pid);
+	if (WIFEXITED(status))
+		global_exit_status(WEXITSTATUS(status));
+	else if WIFSIGNALED(status)
+		global_exit_status(WTERMSIG(status));
 }
